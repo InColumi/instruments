151aa8fe6554fd33b9851f6e8c289e4b541eb12f
Maxim Panov <max77648@gmail.com>
2024-09-09 10:25:44 +0300

    Add schemas

--- python/export_method_from_database/main.py
+++ python/export_method_from_database/main.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 from config import config
 from exporter import Exporter
 from database import DatabaseEgine, Executor
@@ -11,12 +9,13 @@ def export(schemes: tuple, uri: str, path_to_save: str):
     Exporter(executor, schemes).export(path_to_save)
 
 def main():
-    # schemes_medaccount = ('logs', 'public', 'reports', 'wadm')
-    # export(schemes_medaccount,
-    #        config.MEDACCOUNT_DATABASE_URI,
-    #        config.MEDACCOUNT_DIR_FOR_TRACKING_CHANGES)
+    schemes_medaccount = ('logs', 'public', 'reports', 'wadm')
+    export(schemes_medaccount,
+           config.MEDACCOUNT_DATABASE_URI,
+           config.MEDACCOUNT_DIR_FOR_TRACKING_CHANGES)
 
-    schemes_reest_llo = ('frllo',)
+    schemes_reest_llo = ('frllo', 'zayavka')
+
     export(schemes_reest_llo,
            config.REESTR_LLO_DATEBASE_URI,
            config.REESTR_LLO_DIR_FOR_TRACKING_CHANGES)
