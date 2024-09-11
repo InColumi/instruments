ccdb5f92620d4be8b80df7fd6e76081ca70ec032
Maxim Panov <max77648@gmail.com>
2024-09-11 12:30:55 +0300

    Add set for debagging

--- python/export_method_from_database/export_method_from_database.sh
+++ python/export_method_from_database/export_method_from_database.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 source /home/user/Desktop/Projects/instruments/python/export_method_from_database/venv/bin/activate
 echo 'venv activated'
-nice 5 python3 /home/user/Desktop/Projects/instruments/python/export_method_from_database/main.py
+nice -5 python3 /home/user/Desktop/Projects/instruments/python/export_method_from_database/main.py
 
 code '/home/user/Desktop/Projects/scripts_medaccount'
 code '/home/user/Desktop/Projects/Scripts'
